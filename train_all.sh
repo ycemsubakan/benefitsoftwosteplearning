bea0ff00749a8964bb756a561ee6b74212d7cc3d
Maxime Gasse <maxime.gasse@gmail.com>
2019-09-08 21:36:18 -0400

    Updated training script

--- train_all.sh
+++ train_all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-for i in {0..4}
+for k in {10..100..10}
 do
-    echo python train.py $1 $2 -s $i -g $3
-    python train.py $1 $2 -s $i -g $3
+    for i in {0..4}
+    do
+        echo python train.py $1 $k -s $i -g $2
+        python train.py $1 $k -s $i -g $2
+    done
 done
